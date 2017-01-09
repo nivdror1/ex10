ed8f4c84c0bf0216a03595a03e7a2f0eaba7d36c
nivdror1 <nivdror1@gmail.com>
2017-01-09 20:44:50 +0200

    debugging

--- CompilationEngine.java
+++ CompilationEngine.java
@@ -475,7 +475,7 @@ public class CompilationEngine {
            }
          else if (this.currentElement.getTextContent().matches(UNARY_OP)
                 && this.currentElement.getPreviousSibling().
-                getPreviousSibling().getTextContent().matches(OPEN_BRACKETS"|\\s*+(=|&)\\s*+")){
+                getPreviousSibling().getTextContent().matches(OPEN_BRACKETS+"|\\s*+(=|&)\\s*+")){
             addElement(term,SYMBOL); //add the symbol "~,-"
             compileTerm(term);
         }
